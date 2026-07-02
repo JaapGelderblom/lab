882f323a2b2e7a015919633454566f07b2afea3c
JaapGelderblom <jaapgelderblom@gmail.com>
2026-07-02 14:10:10 +0200

    Small add.

--- go-tutorial/array-example.go
+++ go-tutorial/array-example.go
@@ -12,7 +12,13 @@ func main() {
 	//inferred length:
 	var arr3 = [...]int{1, 2, 3}
 
+	arr4 := [5]int{}     //not initialized, default value is 0
+	arr5 := [5]int{1, 2} //partially initialized
+
 	fmt.Println(arr1)
 	fmt.Println(arr2)
 	fmt.Println(arr3)
+	fmt.Println(arr4)
+	fmt.Println(arr5)
+	fmt.Println("the length is: ", len(arr5))
 }
