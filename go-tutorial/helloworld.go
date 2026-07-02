df65a49d05b750915fc4784ee214d9e73516be84
JaapGelderblom <jaapgelderblom@gmail.com>
2026-07-02 10:59:48 +0200

    Some printing examples.

--- go-tutorial/helloworld.go
+++ go-tutorial/helloworld.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	/*some long multiline
+	comment over multiple lines*/
+	var a, b string = "Hello", "World"
+	c, d := "another string", "and another" // type is inferred
+	fmt.Print("Hello World!")
+	fmt.Print(a, b)
+	fmt.Println(a, b)
+	fmt.Println("something else")
+	fmt.Println(c, d)
 }
