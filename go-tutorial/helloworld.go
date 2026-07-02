927ef6f5991fb66e08a912957879bee0935c1d4b
JaapGelderblom <jaapgelderblom@gmail.com>
2026-07-02 11:06:24 +0200

    Adding some number examples.

--- go-tutorial/helloworld.go
+++ go-tutorial/helloworld.go
@@ -14,4 +14,12 @@ func main() {
 	fmt.Println(a, b)
 	fmt.Println("something else")
 	fmt.Println(c, d)
+
+	fmt.Printf("The type of c is: %T and the value is: %v", c, c)
+
+	var i = 15
+
+	fmt.Printf("%b\n", i)  //binair
+	fmt.Printf("%d\n", i)  //decimal
+	fmt.Printf("%+d\n", i) //decimal with sign
 }
